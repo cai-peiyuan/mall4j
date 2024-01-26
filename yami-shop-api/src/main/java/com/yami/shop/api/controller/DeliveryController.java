b200d48531ed7d34d463991b20484ffc0051fcb8
cai-peiyuan <262114450@qq.com>
2024-01-26 13:05:06 +0800

    分享页打开

--- yami-shop-api/src/main/java/com/yami/shop/api/controller/DeliveryController.java
+++ yami-shop-api/src/main/java/com/yami/shop/api/controller/DeliveryController.java
@@ -1,24 +1,21 @@
-
-
 package com.yami.shop.api.controller;
 
-import com.yami.shop.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.yami.shop.common.response.ServerResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import cn.hutool.http.HttpUtil;
 import com.yami.shop.bean.app.dto.DeliveryDto;
 import com.yami.shop.bean.model.Delivery;
 import com.yami.shop.bean.model.Order;
+import com.yami.shop.common.exception.YamiShopBindException;
+import com.yami.shop.common.response.ServerResponseEntity;
 import com.yami.shop.common.util.Json;
 import com.yami.shop.service.DeliveryService;
-
-import cn.hutool.http.HttpUtil;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Parameter;
+import com.yami.shop.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lanhai
@@ -28,28 +25,31 @@ import io.swagger.v3.oas.annotations.Operation;
 @Tag(name = "查看物流接口")
 public class DeliveryController {
 
-	@Autowired
-	private DeliveryService deliveryService;
-	@Autowired
-	private OrderService orderService;
+    @Autowired
+    private DeliveryService deliveryService;
+    @Autowired
+    private OrderService orderService;
 
     /**
      * 查看物流接口
      */
     @GetMapping("/check")
-    @Operation(summary = "查看物流" , description = "根据订单号查看物流")
-    @Parameter(name = "orderNumber", description = "订单号" , required = true)
+    @Operation(summary = "查看物流", description = "根据订单号查看物流")
+    @Parameter(name = "orderNumber", description = "订单号", required = true)
     public ServerResponseEntity<DeliveryDto> checkDelivery(String orderNumber) {
 
-    	Order order = orderService.getOrderByOrderNumber(orderNumber);
-    	Delivery delivery = deliveryService.getById(order.getDvyId());
-    	String url = delivery.getQueryUrl().replace("{dvyFlowId}", order.getDvyFlowId());
-    	String deliveryJson = HttpUtil.get(url);
+        Order order = orderService.getOrderByOrderNumber(orderNumber);
+        if (order == null) {
+            throw new YamiShopBindException("订单不存在");
+        }
+        Delivery delivery = deliveryService.getById(order.getDvyId());
+        String url = delivery.getQueryUrl().replace("{dvyFlowId}", order.getDvyFlowId());
+        String deliveryJson = HttpUtil.get(url);
 
-    	DeliveryDto deliveryDto = Json.parseObject(deliveryJson, DeliveryDto.class);
-    	deliveryDto.setDvyFlowId(order.getDvyFlowId());
-    	deliveryDto.setCompanyHomeUrl(delivery.getCompanyHomeUrl());
-    	deliveryDto.setCompanyName(delivery.getDvyName());
+        DeliveryDto deliveryDto = Json.parseObject(deliveryJson, DeliveryDto.class);
+        deliveryDto.setDvyFlowId(order.getDvyFlowId());
+        deliveryDto.setCompanyHomeUrl(delivery.getCompanyHomeUrl());
+        deliveryDto.setCompanyName(delivery.getDvyName());
         return ServerResponseEntity.success(deliveryDto);
     }
 }
